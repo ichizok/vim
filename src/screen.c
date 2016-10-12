00e6622f063b49d3738c713028788218f094b67a
ichizok <gclient.gaap@gmail.com>
2016-10-12 21:21:36 +0900

    Except GUI

--- src/screen.c
+++ src/screen.c
@@ -8488,7 +8488,11 @@ check_for_delay(int check_msg_scroll)
 screen_valid(int doclear)
 {
     screenalloc(doclear);	   /* allocate screen buffers if size changed */
-    return (ScreenLines != NULL && termcap_active);
+    return (ScreenLines != NULL
+#ifndef FEAT_GUI
+	    && termcap_active
+#endif
+	    );
 }
 
 /*
